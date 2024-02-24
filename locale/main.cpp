576107121f69d351e65f2f324ca1dd16c9b00abb
Mohammad Rahimi <rahimi.mhmmd@gmail.com>
2024-02-24 01:04:52 +0000

    add CommaSeperator facet

--- locale/main.cpp
+++ locale/main.cpp
@@ -28,4 +28,23 @@ int main() {
   auto oldPercision{cout.precision(10)};
   cout << 1.0 / 3.0 << endl;
   cout << setprecision(oldPercision) << 1.0 / 3.0 << endl;
+
+  struct CommaSeperator : ctype<wchar_t> {
+    bool do_is(mask m, char_type c) const {
+      if ((m & space) && c == L' ') {
+        return false;
+      }
+      if ((m & space) && c == L',') {
+        return true;
+      }
+      return ctype::do_is(m, c);
+    }
+  };
+  wcin.imbue(locale{wcin.getloc(), new CommaSeperator{}});
+  wstring wname{};
+  while (wcin >> wname) {
+    if (wname == L"")
+      break;
+    wcout << format(L"Welcome {}!", wname) << endl;
+  }
 }
