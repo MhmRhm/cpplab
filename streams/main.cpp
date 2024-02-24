35c54f239c10e72f45201bff98845ba985204f35
Mohammad Rahimi <rahimi.mhmmd@gmail.com>
2024-02-24 23:25:49 +0000

    add bidirectional binary file manipulations to streams

--- streams/main.cpp
+++ streams/main.cpp
@@ -5,13 +5,20 @@
 #include <iostream>
 #include <iterator>
 #include <limits>
+#include <sstream>
 #include <string>
 #include <vector>
+
+struct Record final {
+  int id{};
+  int partner{};
+  int age{};
+};
 
 int main() {
   using namespace std;
 
-  std::ofstream file{};
+  ofstream file{};
 
   // for files use neutral locale, for UI use system locale
   file.imbue(locale{"C"});
@@ -58,7 +65,6 @@ int main() {
   cout << "Enter participants name: " << endl;
   while (getline(cin, name, '\n')) {
     if (!name.length()) {
-      cout << '[' << name << ']' << "Here 1" << endl;
       break;
     }
     name.erase(name.find_last_not_of(' ') + 1);
@@ -71,8 +77,46 @@ int main() {
 
   party.clear();
 
-  cout << "Enter participants name: " << endl;
-  copy(istream_iterator<string>{cin}, istream_iterator<string>{},
+  istringstream str{"Mohammad Rahimi\nWolf Blitzer\nErin Burnett"};
+  copy(istream_iterator<string>{str}, istream_iterator<string>{},
        back_inserter(party));
   copy(party.begin(), party.end(), ostream_iterator<string>{cout, "\n"});
+
+  cin.ignore();
+
+  auto records = {Record{1, 0, 10}, Record{2, 0, 20}, Record{3, 0, 30},
+                  Record{4, 3, 30}, Record{5, 2, 20}, Record{6, 1, 10}};
+  // 161025203430431052206130
+  fstream document{};
+  document.exceptions(ios::eofbit | ios::badbit | ios::failbit);
+
+  try {
+    document.open("./records", ios::binary | ios::trunc | ios::in | ios::out);
+  } catch (const std::exception &e) {
+    std::cerr << e.what() << '\n';
+  }
+
+  for (auto &&record : records) {
+    document.write(reinterpret_cast<const char *>(&record), sizeof(Record));
+    document.flush();
+    if (record.partner) {
+      document.seekp(sizeof(Record) * (record.partner - 1), ios::beg);
+      document.seekp(offsetof(Record, partner), ios::cur);
+      document.write(reinterpret_cast<const char *>(&record.id),
+                     sizeof(Record::id));
+      document.flush();
+      document.seekp(0, ios::end);
+    }
+  }
+
+  auto size{document.tellp()};
+  document.seekg(0, ios::beg);
+
+  while (document.tellg() < size) {
+    Record record;
+    document.read(reinterpret_cast<char *>(&record), sizeof(Record));
+    cout << format("id: {}, partner: {}, age: {}", record.id, record.partner,
+                   record.age)
+         << endl;
+  }
 }
